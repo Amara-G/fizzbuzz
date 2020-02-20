16e2807d8ac5a4a3529e794be7ea2a2c21692a5f
Amara-G <amaravenkatesh9@dal.ca>
2020-02-20 14:29:11 -0400

    1a

---
+++ task1.py
@@ -0,0 +1,19 @@
+def task_1(file):
+	l = []
+	m = []
+	n = []
+	o = []
+	for i in file:
+		l = i.split(",")
+		#print(l)
+		for each in l:
+			if 'GOOD' == l(9):
+				g.append(l)
+				print(g)
+			elif 'MARGINAL' == l(9)
+				m.append(l)
+				print(m)
+			else ' ' == l(9):
+				e.append(l)
+				print(e)
+task_1(file)
